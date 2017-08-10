855a6e9844adff0e26b85fc8509dbfd004e5ef91
qianyizh <Qianyi.Zhou@gmail.com>
2017-08-10 15:19:24 -0700

    Clean up for Intel Open Source process

--- src/Core/Geometry/Image.cpp
+++ src/Core/Geometry/Image.cpp
@@ -3,8 +3,7 @@
 // ----------------------------------------------------------------------------
 // The MIT License (MIT)
 //
-// Copyright (c) 2017 Qianyi Zhou <Qianyi.Zhou@gmail.com>
-//                    Jaesik Park <syncel@gmail.com>
+// Copyright (c) 2017 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
